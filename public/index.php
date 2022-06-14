b6762cc10d58f329f1263f525fc2cea170dda6c8
PavelKlimovich <pavel@kernl.fr>
2022-06-14 22:30:15 +0200

    mydnex-003: Add Database class.

--- public/index.php
+++ public/index.php
@@ -1,4 +1,12 @@
 <?php
 
+use Dotenv\Dotenv;
+use Src\Database\Database;
+
 require_once "../vendor/autoload.php";
+
+$dotenv = Dotenv::createImmutable("../",'.env');
+$dotenv->load();
+$db = new Database($_ENV['DB_HOST'], $_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+
 require_once "../routes/web.php";
--- src/Database/Database.php
+++ src/Database/Database.php
@@ -2,27 +2,9 @@
 
 namespace Src\Database;
 
-class Database{
+class Database extends PDO
+{
 
-    public static $host = 'localhost';
-    public static $dbName = '';
-    public static $username = '';
-    public static $password = '';
-
-    private static function connect(){
-        $pdo = new \PDO('mysql:host='.self::$host.';dbname='.self::$dbName.';charset=utf8', self::$username, self::$password);
-        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    }
-
-    public static function query($query, $params = array()){
-        $statement = self::connect()->prepare($query);
-        $statement->execute($params);
-        if(explode(' ', $query)[0] == 'SELECT'){
-            $data = $statement->fetchAll();
-            return $data;
-        }
-    }
 }
 
 
